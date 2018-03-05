d74853a87424a667f1b7d8180b97daf74faec250
Hearty Oh <heartyoh@hatiolab.com>
2018-03-05 17:50:03 +0900

    fix component template spec.

--- things-shell.config.js
+++ things-shell.config.js
@@ -1,27 +1,26 @@
 import locales from './locales';
 
+import icon from './assets/half-round-rect.png';
+
 var templates = [{
-  name: 'Half Roundrect', /* 다국어 키 표현을 어떻게.. */
-  description: '...', /* 다국어 키 표현을 어떻게.. */
+  type: 'half round rect',
+  description: 'half round rect',
   group: 'shape', /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
-  icon: '../', /* 또는, Object */
-  template: {
-    type: 'half round rect',
-    model: {
-        type: 'half-roundrect',
-        left: 100,
-        top: 100,
-        width: 100,
-        height: 100,
-        round: 30,
-        fillStyle: '#fff',
-        strokeStyle: '#000',
-        alpha: 1,
-        hidden: false,
-        lineWidth: 1,
-        lineDash: 'solid',
-        lineCap: 'butt'
-    }
+  icon,
+  model: {
+    type: 'half-roundrect',
+    left: 100,
+    top: 100,
+    width: 100,
+    height: 100,
+    round: 30,
+    fillStyle: '#fff',
+    strokeStyle: '#000',
+    alpha: 1,
+    hidden: false,
+    lineWidth: 1,
+    lineDash: 'solid',
+    lineCap: 'butt'
   }
 }];
 
